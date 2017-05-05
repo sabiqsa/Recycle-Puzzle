86160f8a6dbafd26dc61e8d92a34d77a5c3be95e
sabiqsa <sabiqsa@gmail.com>
2017-05-04 21:50:16 -0700

    first

---
+++ Recycle-Puzzle/Assets/Script/Data.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+
+    // Use this for initialization
+    private static int score = 0;
+    public static int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+        }
+    }
+}
+   
---
+++ Recycle-Puzzle/Assets/Script/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+
+    float speed = 0.6f;
+    public Sprite[] img;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = img[Random.Range(0, img.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(0, speed * Time.deltaTime * -1f, 0);
+        if (transform.position.y < -5f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
